54f8cd30f528e3a5f77d11666344b6c4c2bcccf2
niavb <69983201+niavb@users.noreply.github.com>
2020-10-02 19:01:14 +0300

    Add files via upload

---
+++ project07_1.java
@@ -0,0 +1,27 @@
+package edu.smg;
+
+import java.util.Scanner;
+
+public class project07_1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter word:");
+		String word = sc.next();
+		System.out.println("Enter anagram:");
+		String anagram = sc.next();
+		int w = word.length();
+		int a = anagram.length();
+		for(int i=1; i<=w; i++){
+//			int letterInWord = word.charAt(i);
+//			int placeInAnagram = anagram.indexOf(letterInWord);
+			for(int q=1; q<=a; q++){
+				
+			}
+		}
+		
+		sc.close();
+
+	}
+
+}
