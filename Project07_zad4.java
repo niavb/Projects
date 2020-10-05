6aa1bf574731e1608062243a4458e2be005c9ecb
Nia Bakova <69983201+niavb@users.noreply.github.com>
2020-10-05 15:33:04 +0300

    Add files via upload

---
+++ Project07_zad4.java
@@ -0,0 +1,15 @@
+package edu.smg;
+
+public class Project07_zad4 {
+
+	public static void main(String[] args) {
+		for(int j=7; j>=1; j--){
+			for(int i=0; i<j; i++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
