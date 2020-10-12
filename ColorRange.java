e96d50ce4f7f8859d134e8314602ff908236a4a3
Nia Bakova <69983201+niavb@users.noreply.github.com>
2020-10-12 09:20:31 +0300

    Update ColorRange.java

--- ColorRange.java
+++ ColorRange.java
@@ -14,12 +14,12 @@ public class ColorRange {
 		String input = JOptionPane.showInputDialog("Enter a color code:");
 		double wavelenght = Double.parseDouble(input);
 //		System.out.println(wavelenght);
-		if(wavelenght < 450 && wavelenght > 380)System.out.println("The color is Violet");
-		else if(wavelenght < 495 && wavelenght > 450)System.out.println("The color is Blue");
-		else if(wavelenght < 570 && wavelenght > 495)System.out.println("The color is Green");
-		else if(wavelenght < 590 && wavelenght > 570)System.out.println("The color is Yellow");
-		else if(wavelenght < 620 && wavelenght > 590)System.out.println("The color is Orange");
-		else if(wavelenght < 750 && wavelenght > 620)System.out.println("The color is Red");
+		if(wavelenght < 450 && wavelenght >= 380)System.out.println("The color is Violet");
+		else if(wavelenght < 495 && wavelenght >= 450)System.out.println("The color is Blue");
+		else if(wavelenght < 570 && wavelenght >= 495)System.out.println("The color is Green");
+		else if(wavelenght < 590 && wavelenght >= 570)System.out.println("The color is Yellow");
+		else if(wavelenght < 620 && wavelenght >= 590)System.out.println("The color is Orange");
+		else if(wavelenght < 750 && wavelenght >= 620)System.out.println("The color is Red");
 		else System.out.println("The entered wavelength is not a part of the visible spectrum");
 		
 
