a0163e6f3e421601cd52e6b38ffb63486c253610
Nia Bakova <69983201+niavb@users.noreply.github.com>
2020-10-29 17:19:26 +0200

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,15 @@
+package projects11;
+
+public class Student {
+	
+	public String name;
+	public double uspeh;
+	
+	public Student(){	
+	}
+	
+	public void StudentData(String name, double uspeh){
+		this.name = name;
+		this.uspeh = uspeh;	
+	}
+}
