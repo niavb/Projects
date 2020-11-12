51e910c20d3f426b3bbcca889ff460509e554bc6
Nia Bakova <69983201+niavb@users.noreply.github.com>
2020-11-12 16:31:25 +0200

    Add files via upload

---
+++ Projects12_StudentMain.java
@@ -0,0 +1,43 @@
+package projects12;
+
+import java.util.Scanner;
+
+public class Projects12_StudentMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		Student[] students = new Student[5];
+		
+		for(int i=0; i<5; i++) {
+			students[i] = new Student();
+		}
+		
+		for(int i=0; i<5; i++) {
+			System.out.println("Въведете име/успех: ");
+			String name = sc.nextLine();
+			double uspeh = sc.nextDouble();
+			sc.nextLine();
+			students[i] = new Student(name, uspeh);
+		}
+		
+		studentsWithStipendia(students);
+		
+		sc.close();
+
+	}
+	
+	public static boolean hasStipendia(Student student) {
+		return student.getUspeh() >= 5.50;
+	}
+	
+	public static void studentsWithStipendia(Student students[]) {
+		System.out.println("Учениците със стипендии са: ");
+		for(int i=0; i < students.length; i++) {
+			if(hasStipendia(students[i])) {
+				System.out.println(students[i].getName());
+			}
+		}
+	}
+
+}
