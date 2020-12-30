4d75779ad07dc2555fc33d7efe50f78b48c3a0f1
Nia Bakova <69983201+niavb@users.noreply.github.com>
2020-12-30 19:53:04 +0200

    Add files via upload

---
+++ Projects21/ComicBook.java
@@ -0,0 +1,27 @@
+package projects21;
+
+public class ComicBook extends Book {
+	
+	private String characterName;
+	
+	public ComicBook(double regularPrice,
+            String publisher,
+            int yearPublished,
+            String characterName) {
+        super(regularPrice, publisher, yearPublished);
+        setCharacterName(characterName);
+    }
+	
+	public double computeSalePrice(){
+        return super.getRegularPrice() * 0.4;
+    }
+
+	public String getCharacterName() {
+		return characterName;
+	}
+
+	public void setCharacterName(String characterName) {
+		this.characterName = characterName;
+	}
+
+}
